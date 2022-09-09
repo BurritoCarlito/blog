01d2382bff2fe98691d01e060b940a6db5bbbc52
Carlo Tanjuaquio <carlotanjuaquio@gmail.com>
2022-09-09 01:04:45 +0000

    resolved error after submitting form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -19,10 +19,10 @@ class ArticlesController < ApplicationController
     else
       render :new
     end
-    
-    private
-      def article_params
-        params.require(:article).permit(:title, :body)
-      end
   end
+
+  private
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end
 end
