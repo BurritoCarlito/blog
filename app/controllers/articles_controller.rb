bb778883a267fe46d70bf89549152cb558d180df
Carlo Tanjuaquio <carlotanjuaquio@gmail.com>
2022-09-09 00:23:59 +0000

    added new method show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,8 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all #fetches all articles from the db
   end
+
+  def show
+    @article = Article.find(params[:io])
+  end
 end
